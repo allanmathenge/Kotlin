403fefe738abf3c9ae6e206f47762e586604012a
unknown <allanmathenge2.com>
2024-01-27 01:10:32 +0300

    commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,34 @@
+fun main() {
+    println("Hello World!")
+
+    var a: Int = 10
+    var b: Int = 5
+
+    // mathematical
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+
+    println(a%b)
+
+    // Relational
+    println(a>b)
+    println(a<b)
+    println(a>=b)
+    println(a<=b)
+    println(a==b)
+    println(a != 5)
+
+    var x: Int = 20
+    println(x--)
+    println(--x)
+    println(x)
+}
+
+// Classes and objects in kotlin
+// Constructors in kotlin
+// Kotlin - Getters and Setters
+// Inheritance in Kotlin
+// Kotlin Visibility modifiers
+
